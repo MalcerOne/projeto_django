c94fb6c00576bbbd8fc91600e6b41b724142c5de
Rafael Malcervelli <48256253+MalcerOne@users.noreply.github.com>
2021-03-23 16:31:40 -0300

    conceito c quase terminado

--- notes/views.py
+++ notes/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render, redirect
 from .models import Note
 
+def editNote(identif, titulo, content, tag):
+    note = Note.objects.get(id=identif)
+    note.title = titulo
+    note.content = content
+    note.tag = tag
+    note.save()
+
+def deleteNote(identif):
+    note = Note.objects.get(id=identif)
+    note.delete()
+
+
 def index(request):
     if request.method == 'POST':
+        noteid = request.POST.get('edit_note')
         title = request.POST.get('titulo')
         content = request.POST.get('detalhes')
-        Note.objects.create(title=title, content=content)
-        # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
+        tag = request.POST.get('tags')
+
+        deleteid = request.POST.get('delete_id')
+
+        #Delete note
+        if deleteid != None:
+            deleteNote(deleteid)
+        
+        #Edit note
+        elif noteid != None:
+            editNote(noteid, title, content, tag)
+        
+        #Create note
+        else:
+            Note(title=title, content=content, tag=tag).save()
+    
         return redirect('index')
     else:
         all_notes = Note.objects.all()
